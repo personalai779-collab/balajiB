834f4d1fd8977c79079c17b94ad15a3cb93ac5dc
personalai779-collab <personalai779@gmail.com>
2025-09-10 11:41:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import orderRoutes from "./routes/orderRoutes.js";
-
+import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
